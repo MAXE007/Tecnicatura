ffce643466670e1e1ff642bc120419eea696993e
MAXE007 <maceech7@gmail.com>
2023-07-03 19:15:11 -0300

    Clase 6

---
+++ Java/Leccion 1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,15 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+}
+}
---
+++ python/Leccion 2/Ejercicio 1.py
@@ -0,0 +1,9 @@
+'''
+Ejercicio 1: escribir expresion en forma
+algoritmica
+'''
+# a = float(input('Escriba el valor de a: '))
+# b = float(input('Escriba el valor de b: '))
+# c = float(input('Escriba el valor de c: '))
+# resultado = (a ** 3 * (b ** 2 - 2 * a * c)) / (2 * b)
+# print(f'El resultado es: {resultado}')
