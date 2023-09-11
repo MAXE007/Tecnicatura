4fe06bf85ea2f867a3a49589af4903eed5feeadc
maxiechegaray <maceech7@gmail.com>
2023-09-11 20:42:50 -0300

    Commiteamos leccion 3 clases y objetos en Java

--- Java/semestre2/leccionVlasesObjetos/src/Clases/Persona.java
+++ Java/semestre2/leccionVlasesObjetos/src/Clases/Persona.java
@@ -5,4 +5,10 @@ public class Persona {
     //Atributos de la clase
     String nombre;
     String apellido;
+    
+    //Metodo de la clase(acciones)
+    public void obtenerInformación() {
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
 }
