3206937bdee92b9bdb22b048b35d0adac1fd67d4
Maximiliano Echegaray <maceech7@gmail.com>
2023-09-21 20:02:26 -0300

    subiendo ejercicios de Java

--- Java/semestre2/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/semestre2/Aritmetica/src/Operaciones/Aritmetica.java
@@ -7,6 +7,16 @@ public class Aritmetica {
     int a;
     int b;
     
+    //El constructor es un metodo especial
+    public Aritmetica() {   //Constructor 1
+        System.out.println("Se esta ejecutando este constructor numero uno");
+    }
+    //Estamos viendo lo que se llama sobrecarga de constructores
+    public Aritmetica(int a, int b) {   //Constructor 2
+        this.a = a;
+        this.b = b;
+        System.out.println("Se esta ejecutando este constructor numero dos");
+    }
     //Metodo
     public void sumarNumeros() {
         int resultado = a + b;
--- Java/semestre2/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Java/semestre2/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -6,15 +6,31 @@ package Operaciones;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
+        int a = 10; //variables locales
+        int b = 7;  //Memoria stack
+        miMetodo(); //Llamamos el metodo nuevo
         Aritmetica aritmetica1 = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.b = 7;
         aritmetica1.sumarNumeros();
-        
+        //Para almacenar un objeto o atributo se utiliza memoria heap
         int resultado = aritmetica1.sumarConRetorno();
         System.out.println("Resultado: "+resultado);
         
         resultado = aritmetica1.sumarConArgumentos(12, 26);
-        System.out.println("Resultado: "+resultado);
+        System.out.println("Resultado usando argumentos: "+resultado);
+        
+        System.out.println("aritmetica1 a: "+aritmetica1.a);
+        System.out.println("aritmetica2 b: "+aritmetica1.b);
+        
+        Aritmetica aritmetica2 = new Aritmetica(5, 8);
+        System.out.println("aritmetica2 = " + aritmetica2.a);
+        System.out.println("aritmetica2 = " + aritmetica2.b);
+        
+    }
+    
+    public static void miMetodo(){
+        // a = 10; //Una variable esta limitada
+        System.out.println("Aqui hay otro metodo");
     }
 }
