42e8afd314a4ec0f19b3afb1cc58addea5f24e8c
Maximiliano Echegaray <maceech7@gmail.com>
2023-10-25 18:26:43 -0300

    Clase 9 JavaScript

--- Javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ Javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -25,3 +25,43 @@ if( dentroRango >= valMin && dentroRango <= valMax){
 else{
     console.log('Esta fuera del rango establecido')
 }
+
+//Ejercicio : si el padre puede asistir al juego de su hijo
+let vacaciones = true, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+}
+else{
+    console.log("El padre no puede asistir al juego de su hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2)
+let numero = 12;
+resultado2 = numero % 2 == 0 ? "Es un numero PAR" : "Es un numero IMPAR";
+console.log(resultado2)
+
+//Convertir String a Number
+let miNumero = "10";
+console.log(typeof miNumero);
+let edad2 = Number(miNumero);   //Esta es una funcion
+console.log(typeof edad2);
+//Funcion isNaN
+if(isNaN(edad2)){   //No es un numero = is Not a Number(booleano)
+    console.log("Esta variable no contiene solo numeros")
+}
+else{
+    if(edad2 >= 18){
+    console.log("Puede votar");
+    }
+    else{
+    console.log("Muy joven para votar");
+    }
+}
+
+//Operador ternario
+let resultado3 = edad2 >= 18 ? "Puede votar" : "Muy joven para votar";
+console.log(resultado3);
+
+
